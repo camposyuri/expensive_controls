40d096573829b720c7fe82070056bb0e50140ba2
Yuri Campos <yurinapoleao@gmail.com>
2022-09-26 07:31:10 -0300

    Criação da tabela de endereço

--- src/database/migrations/sqls/20220329231757-initialize-up.sql
+++ src/database/migrations/sqls/20220329231757-initialize-up.sql
@@ -14,7 +14,7 @@ CREATE TABLE IF NOT EXISTS users (
   dateCreated TIMESTAMP NOT NULL,
   status BOOLEAN NOT NULL DEFAULT TRUE,
   admin BOOLEAN NOT NULL DEFAULT FALSE,
-  CONSTRAINT pk_iduser PRIMARY KEY(id)
+  CONSTRAINT pk_id_user PRIMARY KEY(id)
 );
 
 DROP SEQUENCE IF EXISTS person_idperson_seq;
@@ -35,7 +35,8 @@ CREATE TABLE IF NOT EXISTS person (
 	telephone VARCHAR(20),
 	phone VARCHAR(20) NOT NULL,
 	dateCreated TIMESTAMP NOT NULL,
-	status BOOLEAN NOT NULL DEFAULT TRUE
+	status BOOLEAN NOT NULL DEFAULT TRUE,
+	CONSTRAINT pk_id_person PRIMARY KEY(id)
 );
 
 DROP SEQUENCE IF EXISTS provider_idprovider_seq;
@@ -55,7 +56,8 @@ CREATE TABLE IF NOT EXISTS provider (
 	dateCreated TIMESTAMP NOT NULL,
 	status BOOLEAN NOT NULL DEFAULT TRUE,
 	telephone VARCHAR(20),
-	phone VARCHAR(20) NOT NULL
+	phone VARCHAR(20) NOT NULL,
+	CONSTRAINT pk_id_provider PRIMARY KEY(id)
 );
 
 DROP SEQUENCE IF EXISTS customer_idcustomer_seq;
@@ -75,5 +77,30 @@ CREATE TABLE IF NOT EXISTS customer (
 	dateCreated TIMESTAMP NOT NULL,
 	status BOOLEAN NOT NULL DEFAULT TRUE,
 	telephone VARCHAR(20),
-	phone VARCHAR(20) NOT NULL
+	phone VARCHAR(20) NOT NULL,
+	CONSTRAINT pk_id_customer PRIMARY KEY(id)
+);
+
+
+DROP SEQUENCE IF EXISTS address_idaddress_seq;
+CREATE SEQUENCE address_idaddress_seq
+								INCREMENT 1
+								MINVALUE 1
+								MAXVALUE 9223372036854775807
+								START 1
+								CACHE 1;
+
+CREATE TABLE IF NOT EXISTS address (
+	id INTEGER NOT NULL DEFAULT nextval('address_idaddress_seq'),
+	publicPlace VARCHAR(250) NOT NULL,
+	number INTEGER NOT NULL,
+	complement VARCHAR(150),
+	district VARCHAR(100) NOT NULL,
+	county VARCHAR(100) NOT NULL,
+	zipCode VARCHAR(9) NOT NULL,
+	uf CHAR(2) NOT NULL,
+	CONSTRAINT pk_id_address PRIMARY KEY(id),
+	CONSTRAINT fk_id_customer FOREIGN KEY (id) REFERENCES customer(id),
+	CONSTRAINT fk_id_person FOREIGN KEY (id) REFERENCES person(id),
+	CONSTRAINT fk_id_provider FOREIGN KEY (id) REFERENCES provider(id)
 );
