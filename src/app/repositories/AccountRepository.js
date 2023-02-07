2425799cc2cadd53eec1794221b5c0b3b96f35c5
Yuri Campos <yurinapoleao@gmail.com>
2023-02-07 07:52:01 -0300

    refactor: Adjusted endpoint account

--- src/app/repositories/AccountRepository.js
+++ src/app/repositories/AccountRepository.js
@@ -6,26 +6,55 @@ class AccountRepository {
 		try {
 			const direction = orderBy.toUpperCase() === "DESC" ? "DESC" : "ASC";
 
-			const rows = await db.query(
-				`
-					SELECT
-						id ,
-						id_customer ,
-						id_provider ,
-						id_person ,
-						id_account_classification ,
-						id_account_type ,
-						"name" ,
-						value ,
-						expiration_date ,
-						payment_date ,
-						datecreated ,
-						status
-					FROM
-						account a
-					ORDER BY id ${direction};
-				`
-			);
+			const rows = await db.query(`
+				SELECT
+					a.id ,
+					a.id_customer as codCustomer,
+					c.fantasyname as customerName,
+					a.id_provider as codProvider,
+					p.corporatename as providerName,
+					a.id_person as codPerson,
+					p2."name" as personName,
+					a.id_account_classification ,
+					ac.decription as descriptionAccount,
+					a.id_account_type ,
+					at2.decription as descriptionType,
+					a."name" ,
+					a.value ,
+					a.expiration_date ,
+					a.payment_date ,
+					a.datecreated ,
+					a.status
+				FROM
+					account a
+				LEFT JOIN customer c ON c.id = a.id_customer
+				LEFT JOIN provider p ON p.id = a.id_provider
+				LEFT JOIN person p2 ON p2.id = a.id_person
+				INNER JOIN account_classification ac ON ac.id = a.id_account_classification
+				INNER JOIN account_type at2 ON at2.id = a.id_account_type
+				ORDER BY a.id ${direction};
+			`);
+
+			// const rows = await db.query(
+			// 	`
+			// 		SELECT
+			// 			id ,
+			// 			id_customer ,
+			// 			id_provider ,
+			// 			id_person ,
+			// 			id_account_classification ,
+			// 			id_account_type ,
+			// 			"name" ,
+			// 			value ,
+			// 			expiration_date ,
+			// 			payment_date ,
+			// 			datecreated ,
+			// 			status
+			// 		FROM
+			// 			account a
+			// 		ORDER BY id ${direction};
+			// 	`
+			// );
 
 			return rows;
 		} catch (error) {
