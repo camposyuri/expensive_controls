0b8e6d567ff350cd25b54855f200aa6d6b9db20a
Yuri Campos <yurinapoleao@gmail.com>
2023-04-07 10:56:03 -0300

    feat: Disabled CORS

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -1,6 +1,6 @@
 // Middleware de CORS para todoas as rotas da API.
 module.exports = (request, response, next) => {
-	response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+	response.setHeader("Access-Control-Allow-Origin", "*");
 	response.setHeader("Access-Control-Allow-Methods", "*");
 	response.setHeader("Access-Control-Allow-Headers", "*");
 	response.setHeader("Access-Control-Max-Age", "10");
