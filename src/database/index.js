c681a344c3a3a9b5f6d8c0e6958d7c69ec5e3223
Yuri Campos <yurinapoleao@gmail.com>
2023-04-10 13:42:01 -0300

    feat: npm build

--- src/database/index.js
+++ src/database/index.js
@@ -18,26 +18,13 @@ const { Client } = require("pg");
 // });
 
 const client = new Client({
-	connectionString:
-		"postgres://expensive_control_user:cjatqHaqgNwqEbnbdvplD51A2SYDVYPX@dpg-cgom3r0u9tun42pniu5g-a/expensive_control",
+	host: process.env.DB_HOST,
+	port: process.env.DB_PORT,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_DATABASE,
+	ssl: process.env.SSL,
 });
-
-// host: process.env.DB_HOST,
-// 	port: process.env.DB_PORT,
-// 	user: process.env.DB_USER,
-// 	password: process.env.DB_PASSWORD,
-// 	database: process.env.DB_DATABASE,
-// 	ssl: process.env.SSL,
-// "dev": {
-// 	"driver": "pg",
-// 	"user": { "ENV": "DB_USER" },
-// 	"password": { "ENV": "DB_PASSWORD" },
-// 	"host": { "ENV": "DB_HOST" },
-// 	"database": { "ENV": "DB_DATABASE" },
-// 	"port": { "ENV": "DB_PORT" },
-// 	"schema": { "ENV": "DB_SCHEMA" },
-// 	"ssl": { "ENV": "SSL" }
-// }
 
 client.connect();
 
