60f53cf3395c19763a26bf3e8928a3bd47596f6c
Yuri Campos <yurinapoleao@gmail.com>
2023-04-10 12:12:31 -0300

    feat: npm build

--- src/database/index.js
+++ src/database/index.js
@@ -1,9 +1,23 @@
-const { Pool } = require("pg");
+// const { Pool } = require("pg");
 require("dotenv").config();
 
-// const { Client, Pool } = require("pg");
+const { Client } = require("pg");
 
-const pool = new Pool({
+// const pool = new Pool({
+// 	host: process.env.DB_HOST,
+// 	port: process.env.DB_PORT,
+// 	user: process.env.DB_USER,
+// 	password: process.env.DB_PASSWORD,
+// 	database: process.env.DB_DATABASE,
+// 	ssl: process.env.SSL,
+// });
+
+// const pool = new Pool({
+// 	connectionString: process.env.DATABASE_URL,
+// 	ssl: process.env.SSL,
+// });
+
+const client = new Client({
 	host: process.env.DB_HOST,
 	port: process.env.DB_PORT,
 	user: process.env.DB_USER,
@@ -12,23 +26,9 @@ const pool = new Pool({
 	ssl: process.env.SSL,
 });
 
-// const pool = new Pool({
-// 	connectionString: process.env.DATABASE_URL,
-// 	ssl: process.env.SSL,
-// });
-
-// const client = new Client({
-// 	host: process.env.DB_HOSTNAME,
-// 	port: process.env.DB_PORT,
-// 	user: process.env.DB_USER,
-// 	password: process.env.DB_PASSWORD,
-// 	database: process.env.DB_DATABASE,
-// 	ssl: true,
-// });
-
-pool.connect();
+client.connect();
 
 exports.query = async (query, values) => {
-	const { rows } = await pool.query(query, values);
+	const { rows } = await client.query(query, values);
 	return rows;
 };
