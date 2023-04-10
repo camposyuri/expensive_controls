1cfbb98d4d781bcfdc0dde3f209160a62831bd06
Yuri Campos <yurinapoleao@gmail.com>
2023-04-10 14:08:54 -0300

    feat: npm build

--- src/database/index.js
+++ src/database/index.js
@@ -18,12 +18,8 @@ const { Client } = require("pg");
 // });
 
 const client = new Client({
-	host: process.env.DB_HOST,
-	port: process.env.DB_PORT,
-	user: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DB_DATABASE,
-	ssl: process.env.SSL,
+	connectionString: process.env.DATABASE_URL,
+	ssl: true,
 });
 
 console.log(client);
