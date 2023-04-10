bbe7e6dd389492b76b0632dfadefc6d14836e5d4
Yuri Campos <45168468+camposyuri@users.noreply.github.com>
2023-04-10 10:51:36 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const router = require("./routes");
 const cors = require("./middlewares/cors");
 const app = express();
 
-const port = process.env.REDIS_PORT || 3001;
+const port = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(cors);
