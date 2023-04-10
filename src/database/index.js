3746cff970346fa34ae87de6360a60049696e026
Yuri Campos <yurinapoleao@gmail.com>
2023-04-10 11:24:09 -0300

    feat: npm build

--- src/database/index.js
+++ src/database/index.js
@@ -9,7 +9,7 @@ const pool = new Pool({
 	user: process.env.DB_USER,
 	password: process.env.DB_PASSWORD,
 	database: process.env.DB_DATABASE,
-	ssl: true,
+	ssl: process.env.SSL,
 });
 
 // const client = new Client({
