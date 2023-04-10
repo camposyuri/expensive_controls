fd21d99e4a5f8e64019290f9848e8367b3a8fe0f
Yuri Campos <yurinapoleao@gmail.com>
2023-04-10 12:01:55 -0300

    feat: npm build

--- src/database/index.js
+++ src/database/index.js
@@ -3,30 +3,20 @@ require("dotenv").config();
 
 // const { Client, Pool } = require("pg");
 
-// const pool = new Pool({
-// 	host: process.env.DB_HOSTNAME,
-// 	port: process.env.DB_PORT,
-// 	user: process.env.DB_USER,
-// 	password: process.env.DB_PASSWORD,
-// 	database: process.env.DB_DATABASE,
-// 	ssl: process.env.SSL,
-// });
-
 const pool = new Pool({
-	connectionString: process.env.DATABASE_URL,
+	host: process.env.DB_HOSTNAME,
+	port: process.env.DB_PORT,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_DATABASE,
 	ssl: process.env.SSL,
 });
 
-// "dev": {
-// 	"driver": "pg",
-// 	"user": { "ENV": "DB_USER" },
-// 	"password": { "ENV": "DB_PASSWORD" },
-// 	"host": { "ENV": "DB_HOSTNAME" },
-// 	"database": { "ENV": "DB_DATABASE" },
-// 	"port": { "ENV": "DB_PORT" },
-// 	"schema": { "ENV": "DB_SCHEMA" },
-// 	"ssl": { "ENV": "SSL" }
-// },
+// const pool = new Pool({
+// 	connectionString: process.env.DATABASE_URL,
+// 	ssl: process.env.SSL,
+// });
+
 // const client = new Client({
 // 	host: process.env.DB_HOSTNAME,
 // 	port: process.env.DB_PORT,
